1bfcaf5b1364c110c67ae4c722ac664fa2d16d8d
Thatcher <thcraig@davidson.edu>
2022-12-23 23:24:01 -0500

    login verification
    nope

--- my_project/app.py
+++ my_project/app.py
@@ -1,40 +1,64 @@
-import mc_recommendations as mc_recommendations
-from flask import Flask, render_template, request
-from flask_cors import CORS, cross_origin
+from flask import Flask, redirect, url_for, request, session, render_template
+import requests
 
 app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADER'] = 'Content-Type'
-#revert
-@app.route('/form')
-@cross_origin()
-def show_form():
-    return render_template('form.html')
+app.secret_key = "barryisgay"
+# Replace these with your own client ID and client secret
+CLIENT_ID = "c1f4309625494c848f3d90c0b3f96813"
+CLIENT_SECRET = "7b676d5c061c439389d95257530a0a76"
 
-    
+# This is the URL of your app's homepage
+REDIRECT_URI = "http://localhost:5000/callback"
+
+# This is the URL that the user will be redirected to after they grant
+# permission to your app
+CALLBACK_URI = "http://localhost:5000/home"
+
+@app.route("/")
+def homepage():
+    return render_template("login.html")
+
+@app.route("/login")
+def login():
+    # Generate a URL that the user can use to grant permission to your app
+    scope = "user-read-private user-read-email"
+    url = f"https://accounts.spotify.com/authorize?client_id={CLIENT_ID}&response_type=code&redirect_uri={REDIRECT_URI}&scope={scope}"
+    return redirect(url)
+
+@app.route("/callback")
+def callback():
+    # Get the authorization code from the query string
+    code = request.args.get("code")
+
+    # Use the authorization code to request an access token
+    data = {
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": REDIRECT_URI,
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+    }
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    response = requests.post("https://accounts.spotify.com/api/token", data=data, headers=headers)
+    token = response.json()["access_token"]
+
+    # Save the access token in a session cookie
+    session["access_token"] = token
+
+    # Redirect the user to the app's homepage
+    return redirect(url_for("home"))
 
 
-@app.route('/process', methods=['POST', 'GET'])
-@cross_origin()
-def process_input():
-    #print("here")
-    #print(request.form)
-    input_value = request.form['input']
-    
-    parts = input_value.split('/')
-    playlist_id = parts[-1]
-    parts = playlist_id.split('?')
-    playlist_id = parts[0]
+@app.route("/home")
+def home():
+    # Get the access token from the session cookie
+    token = session.get("access_token")
 
-    
+    # Use the access token to authenticate the user and make API requests
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.get("https://api.spotify.com/v1/me", headers=headers)
+    user = response.json()
+    return f"Welcome, {user['display_name']}!"
 
-    results = mc_recommendations.give_me_recs(input_value)
-    df_json = results.to_json()
-    #print(len(df_json))
-    return df_json
-
-
-
-
-if __name__ == 'main':
+if __name__ == "__main__":
     app.run()
--- my_project/venv/bin/activate.fish
+++ my_project/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/thatcher/Documents/MilkCrateBackend/venv"
+set -gx VIRTUAL_ENV "/Users/thatcher/Documents/MilkCrateBackend/my_project/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
